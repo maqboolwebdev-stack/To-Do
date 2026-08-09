d59422b42d9e3f87ffd8c829a19b699c539d55b9
Waqas Maqbool <maqbool.webdev@gmail.com>
2026-08-09 20:57:59 +0500

    Add link of all modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import "./styles/styles.css";
-import "./styles/css-resets.css";
-
+import './styles/styles.css';
+import './styles/css-resets.css';
+import './modules/projects.js';
+import './modules/project-dialog.js';
+import './styles/project-dialog.css';
+import './modules/projects-ui.js';
