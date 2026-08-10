1762f84d573cdd4434227af4d48be1677c9a0ef5
Waqas Maqbool <maqbool.webdev@gmail.com>
2026-08-10 18:04:09 +0500

    Render todos dynamically

---
+++ src/modules/render-todos.js
@@ -0,0 +1,47 @@
+export const todoSide = document.querySelector('.todos-side');
+
+export function renderTodos(project) {
+  todoSide.innerHTML = '';
+
+  
+  project.todo.forEach(todo => {
+
+    const todoContainer = document.createElement('div');
+    const todoCheckbox = document.createElement('input');
+    const todoTitle = document.createElement('div');
+    const todoDescription = document.createElement('div');
+    const todoPriority = document.createElement('div');
+    const selectPriority = document.createElement('span');
+    const todoDueDate = document.createElement('div');
+    const todoEditBtn = document.createElement('button');
+    const todoDeleteBtn = document.createElement('button');
+
+    todoCheckbox.type = 'checkbox'
+
+    todoContainer.classList = 'todos';
+    todoCheckbox.classList = 'todo-checkbox';
+    todoTitle.classList = 'todo-title';
+    todoDescription.classList = 'todo-desc';
+    todoPriority.classList = 'todo-priority';
+    todoDueDate.classList = 'todo-dueDate';
+    todoEditBtn.classList.add('btn');
+    todoEditBtn.classList.add('btn-edit');
+    todoDeleteBtn.classList.add('btn');
+    todoDeleteBtn.classList.add('btn-delete');
+
+    todoTitle.textContent = todo.title;
+    todoDescription.textContent = todo.description;
+    todoPriority.textContent = 'Priority: ';
+    selectPriority.textContent = todo.priority;
+    todoDueDate.textContent = todo.dueDate;
+    todoEditBtn.textContent = 'Edit';
+    todoDeleteBtn.textContent = 'Delete';
+
+    todoPriority.append(selectPriority);
+    todoContainer.append(todoCheckbox, todoTitle, todoDescription, todoPriority, todoDueDate, todoEditBtn, todoDeleteBtn);
+    todoSide.append(todoContainer);
+
+    console.log(todo.title);
+  })
+
+}
