0caf13fe56c1fae7e13efbf82e99b979900295f1
Waqas Maqbool <maqbool.webdev@gmail.com>
2026-08-10 20:17:21 +0500

    Add dummy projects and todos

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -6,12 +6,11 @@ class Project {
 }
 
 class Todo {
-    constructor(title, description, dueDate, priority, completed) {
+    constructor(title, description, dueDate, priority) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
-        this.completed = false;
     }
 }
 
@@ -31,9 +30,14 @@ function createTodo(project, title, description, dueDate, priority) {
 }
 
 createProject('Default Project');
+createProject('Office');
+createProject('Do the best');
 
-createTodo(projects[0], 'homework', 'bla bla bla bal bal', '23/3/2027', 'High',);
-createTodo(projects[0], 'cooking', 'bla bla bla bal bal', '23/3/2027', 'low',);
-createTodo(projects[0], 'gym', 'bla bla bla bal bal', '23/3/2027', 'medium',);
+createTodo(projects[0], 'cooking', 'I love cooking, I am great in cooking.', '23/3/2027', 'low',);
+createTodo(projects[0], 'gym', 'Doing Gym is the best habit for our health.', '23/3/2027', 'medium',);
+
+createTodo(projects[1], 'milk', 'I like drinking very much. Milk is a good source of good fats protein and lots of vitamins and minerals', '23/3/2027', 'medium',);
+createTodo(projects[1], 'homework', 'bla bla bla bal bal bla bla bla bla', '23/3/2027', 'High',);
+createTodo(projects[2], 'curd', 'Curd same as Milk but curd have good bacteria that support gut health.', '23/3/2027', 'medium',);
 
 export {projects, createProject};
